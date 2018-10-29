163e93001c94d6e6e29e7b7b9233bcf13b1631e2
Gabriel Podolan <gabriel.podolan@zalando.de>
2018-10-29 02:22:41 +0100

    Including scripts and plots

---
+++ plot1.R
@@ -0,0 +1,20 @@
+library(data.table)
+library(lubridate)
+
+## Check if files exist, download and unzip
+if(!file.exists("household_power_consumption.zip")){ 
+    download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", destfile = "household_power_consumption.txt")
+    unzip(zipfile = "household_power_consumption.zip", files = "household_power_consumption.txt", overwrite = T)
+}
+
+## Load data into R and convert dates to Date format
+powerConsumption <- fread(file = "household_power_consumption.txt", na.strings = "?")
+powerConsumption$Date <- dmy(powerConsumption$Date)
+
+## Select relevant data
+relevantData <- powerConsumption[powerConsumption$Date >= "2007-02-01" & Date <= "2007-02-02",]
+
+## Create the device, write the plot and close the device
+png(filename = "plot1.png")
+hist(relevantData$Global_active_power, col = "red", xlab = "Global Active Power (kilowatts)", main = "Global Active Power")
+dev.off()
---
+++ plot3.R
@@ -0,0 +1,24 @@
+library(data.table)
+library(lubridate)
+
+## Check if files exist, download and unzip
+if(!file.exists("household_power_consumption.zip")){ 
+    download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", destfile = "household_power_consumption.txt")
+    unzip(zipfile = "household_power_consumption.zip", files = "household_power_consumption.txt", overwrite = T)
+}
+
+## Load data into R and convert dates to Date format
+powerConsumption <- fread(file = "household_power_consumption.txt", na.strings = "?")
+powerConsumption$Date <- dmy(powerConsumption$Date)
+
+## Select relevant data and generate DateTime column in POSIXct formathead
+relevantData <- powerConsumption[powerConsumption$Date >= "2007-02-01" & Date <= "2007-02-02",]
+relevantData <- mutate(relevantData, DateTime = as.POSIXct(paste(relevantData$Date, relevantData$Time)))
+
+## Create the device, write the plot and close the device
+png(filename = "plot3.png")
+with(relevantData, plot(DateTime, Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering"))
+with(relevantData, lines(Sub_metering_2 ~ DateTime, type = "l", col = "red"))
+with(relevantData, lines(Sub_metering_3 ~ DateTime, type = "l", col = "blue"))
+legend("topright", legend = names(relevantData)[grepl("^Sub_", names(relevantData))], col = c("black", "red", "blue"), lty = c(1, 1, 1))
+dev.off()
