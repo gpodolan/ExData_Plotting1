fbc8c3f9d7c5851058837f53fc4b97631225faf1
Gabriel Podolan <gpodolan@gmail.com>
2018-10-29 02:28:59 +0100

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -4,11 +4,11 @@ library(lubridate)
 ## Check if files exist, download and unzip
 if(!file.exists("household_power_consumption.zip")){ 
     download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", destfile = "household_power_consumption.txt")
-    unzip(zipfile = "household_power_consumption.zip", files = "household_power_consumption.txt", overwrite = T)
+    file <- unzip(zipfile = "household_power_consumption.zip", files = "household_power_consumption.txt", overwrite = T)
 }
 
 ## Load data into R and convert dates to Date format
-powerConsumption <- fread(file = "household_power_consumption.txt", na.strings = "?")
+powerConsumption <- fread(file, na.strings = "?")
 powerConsumption$Date <- dmy(powerConsumption$Date)
 
 ## Select relevant data and generate DateTime column in POSIXct formathead
